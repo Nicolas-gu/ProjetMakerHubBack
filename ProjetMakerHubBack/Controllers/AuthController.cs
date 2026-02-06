6e9009db318e0465e43bb0eb8557d212ecdfcd8f
Nicolas G <nicolas.gueb.dev@gmail.com>
2026-02-06 11:35:48 +0100

    authcontroller

--- ProjetMakerHubBack/Controllers/AuthController.cs
+++ ProjetMakerHubBack/Controllers/AuthController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetMakerHubBack.API.Dto;
+using ProjetMakerHubBack.Application.Services;
+using System.Security.Authentication;
 
 namespace ProjetMakerHubBack.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuthController : ControllerBase
+    public class AuthController(AuthService _authService) : ControllerBase
     {
-        public IActionResult Login()
+        [HttpPost("login")]
+        [Consumes(typeof(LoginRequestDTO), "application/json")]
+        [EndpointSummary("Authenticates a user and returns a JWT token.")]
+        public IActionResult Login([FromBody] LoginRequestDTO dto)
         {
-            
+            try
+            {
+                string token = _authService.Login(dto.Email, dto.Password);
+                return Ok(new { token });
+            }
+            catch (AuthenticationException)
+            {
+                return Unauthorized();
+            }
         }
     }
 }
